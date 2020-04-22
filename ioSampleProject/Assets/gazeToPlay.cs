768037a3ee89be6acb724bdf4d8fb0fe4497a8f1
naxwell <maxwell.lander@gmail.com>
2020-04-22 10:27:33 -0400

    annotations

--- ioSampleProject/Assets/gazeToPlay.cs
+++ ioSampleProject/Assets/gazeToPlay.cs
@@ -14,12 +14,14 @@ public class gazeToPlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // vreate a raycast
         RaycastHit hit;
         
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
-        {
+        {  
+            //Check if raycast hits a depthkit clip
             if(hit.collider.gameObject.tag == "depthClip"){
+                //if yes, play said clip
                 hit.collider.gameObject.GetComponent<UnityEngine.Video.VideoPlayer>().Play();
             }
         }

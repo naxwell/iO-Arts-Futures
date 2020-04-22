85d253f65e9d480dd43c2d95effd1a6ea42aefd3
naxwell <maxwell.lander@gmail.com>
2020-04-22 10:32:18 -0400

    annotation

--- ioSampleProject/Assets/pauseAtStart.cs
+++ ioSampleProject/Assets/pauseAtStart.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
 
+// This script pauses the Depthkit clip at the beginning of the experience. This ensures he correct clip is visible in the scene and prepares it to be played by one of the other interaction scripts
 public class pauseAtStart : MonoBehaviour
 {
 
@@ -12,6 +13,7 @@ public class pauseAtStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //associates the connected videoplayer and runs the coroutine
         vp = GetComponent<VideoPlayer>();
         StartCoroutine(initPause());
     }
@@ -22,7 +24,7 @@ public class pauseAtStart : MonoBehaviour
         
     }
      
-    
+    // A coroutine is used in order to put in a slight pause, ensuring the clip has time to load correctly before pausing 
     IEnumerator initPause()
     {
         yield return new WaitForSeconds(1);

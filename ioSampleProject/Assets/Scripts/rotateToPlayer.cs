071a1c30b1061cca3a66f703e60ecfbd67e7ac2e
naxwell <maxwell.lander@gmail.com>
2020-04-22 10:48:17 -0400

    annotation

--- ioSampleProject/Assets/Scripts/rotateToPlayer.cs
+++ ioSampleProject/Assets/Scripts/rotateToPlayer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// This script turns the depthkit clip to face the place always. Must be placed on the container parent object of the video clip. 
 public class rotateToPlayer : MonoBehaviour
 {
 
